6b623d56a6d95a135125b5480f9611c572f2ae57
Gaetano Perrone <gaetano.perrone@epsilonline.com>
2018-09-25 11:30:05 +0200

    Change hostname template word in order to return only hostname, add url template word

--- examples/tl.js
+++
@@ -1,6 +0,0 @@
-labs = require('../app/data/labels.js') 
-
-
-labs.getLabels('/Users/gaetanoperrone/dsp/giper_repo/labels.json', function(err, obj) {
-	console.log(err)
-})
--- public/app/services/cleaner_service.js
+++ public/app/services/cleaner_service.js
@@ -1,7 +1,8 @@
 function dsp_cleanerService($location) {
 
 this.parse = function parse(text) {
-    text = text.replace(/\{\{hostname\}\}/g, $location.protocol() + "://" + $location.host())
+    text = text.replace(/\{\{hostname\}\}/g, $location.host())
+    text = text.replace(/\{\{url\}\}/g, $location.protocol() + "://" + $location.host())
     return text
   }
 }
