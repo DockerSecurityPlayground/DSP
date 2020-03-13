a35ef907bf0cfa5114e5eec8ecf061347f591b02
catuhub <efrcatu@gmail.com>
2020-03-13 19:36:41 +0000

    Added structure for GET snippets (endpoint + handler + data)

---
+++ app/handlers/snippets.js
@@ -0,0 +1,27 @@
+const snippetsData = require('../data/snippets.js');
+const path = require('path');
+const httpHelper = require('help-nodejs').httpHelper;
+
+const Checker = require('../util/AppChecker.js');
+const configData = require('../data/config.js');
+const AppUtils = require('../util/AppUtils.js');
+const c = require('../../config/local.config.json');
+
+function allSnippets(req, res){
+    async.waterfall([
+        (cb) => Checker.checkParams(req.params, ['repo'], cb),
+        (cb) => {configData.getConfig(cb);},
+
+        (config, cb) => {
+            const mainDir = config.mainDir;
+
+            const snippetsFile = path.join(AppUtils.getHome(), mainDir, req.params.repo, c.config.name_snippetsfile)
+            snippetsData.getSnippets(snippetsFile,cb);
+        },
+    ],
+    (err, results) => {
+        httpHelper.response(res, err, {snippets: results});
+    });
+}
+
+exports.allSnippets = allSnippets;
