1b7d7a64c97f49629876e2840cc75727f8b9ea0f
MassimilianoCaruso <80095084+MassimilianoCaruso@users.noreply.github.com>
2021-10-09 00:09:27 +0200

    Create steppers.js

---
+++ public/assets/md-steppers/src/js/steppers.js
@@ -0,0 +1,9 @@
+/*
+* Credit: Credit to angular md-tabs
+*         All codes are based on md-tabs by angular material
+* Reference: https://github.com/angular/material
+*/
+
+angular.module('md-steppers', [
+  'material.core',
+  'material.components.icon']);
